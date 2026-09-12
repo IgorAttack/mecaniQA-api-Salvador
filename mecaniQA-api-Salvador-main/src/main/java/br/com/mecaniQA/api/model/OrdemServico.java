7ecdec4b25b6ae0e0e1d3eaa42f5c6ec6e42ef25
icarovinhas <icaromvinhas@gmail.com>
2026-09-11 23:20:35 -0300

    Atualização
    Quando Feito O Código Estava Com Problemas Nas Chaves Fazendo O Código Não Rodar E Agora Corrigido E Rodando Para Fazer O Teste Para O QA

--- mecaniQA-api-Salvador-main/src/main/java/br/com/mecaniQA/api/model/OrdemServico.java
+++ mecaniQA-api-Salvador-main/src/main/java/br/com/mecaniQA/api/model/OrdemServico.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class OrdemServico {
 
-    private Builder codigoUnico;
+    private Long codigoUnico;
     private String descricaoProblema;
     private StatusOrdemServico status;
     private Double valorTotal;
@@ -16,6 +16,7 @@ public class OrdemServico {
     private List<Servico> servicos;
     private List<PedidoPecas> pedidosPecas;
 
+    // Construtor privado
     private OrdemServico(Builder builder) {
         this.codigoUnico = builder.codigoUnico;
         this.descricaoProblema = builder.descricaoProblema;
@@ -28,12 +29,15 @@ public class OrdemServico {
         this.pedidosPecas = builder.pedidosPecas;
     }
 
+    // Método para iniciar o Builder
     public static Builder builder() {
         return new Builder();
     }
 
+    // Builder
     public static class Builder {
-        private Builder codigoUnico;
+
+        private Long codigoUnico;
         private String descricaoProblema;
         private StatusOrdemServico status;
         private Double valorTotal;
@@ -42,127 +46,128 @@ public class OrdemServico {
         private LocalDateTime dataUltimaAtualizacao;
         private List<Servico> servicos = new ArrayList<>();
         private List<PedidoPecas> pedidosPecas = new ArrayList<>();
+
+        public Builder codigoUnico(Long codigoUnico) {
+            this.codigoUnico = codigoUnico;
+            return this;
+        }
+
+        public Builder descricaoProblema(String descricaoProblema) {
+            this.descricaoProblema = descricaoProblema;
+            return this;
+        }
+
+        public Builder status(StatusOrdemServico status) {
+            this.status = status;
+            return this;
+        }
+
+        public Builder valorTotal(Double valorTotal) {
+            this.valorTotal = valorTotal;
+            return this;
+        }
+
+        public Builder dataAbertura(LocalDateTime dataAbertura) {
+            this.dataAbertura = dataAbertura;
+            return this;
+        }
+
+        public Builder dataFechamento(LocalDateTime dataFechamento) {
+            this.dataFechamento = dataFechamento;
+            return this;
+        }
+
+        public Builder dataUltimaAtualizacao(LocalDateTime dataUltimaAtualizacao) {
+            this.dataUltimaAtualizacao = dataUltimaAtualizacao;
+            return this;
+        }
+
+        public Builder servicos(List<Servico> servicos) {
+            this.servicos = servicos;
+            return this;
+        }
+
+        public Builder pedidosPecas(List<PedidoPecas> pedidosPecas) {
+            this.pedidosPecas = pedidosPecas;
+            return this;
+        }
+
+        public OrdemServico build() {
+            return new OrdemServico(this);
+        }
     }
 
-    public Builder codigoUnico(Long codigoUnico) {
+    // Getters e Setters
+
+    public Long getCodigoUnico() {
+        return codigoUnico;
+    }
+
+    public void setCodigoUnico(Long codigoUnico) {
         this.codigoUnico = codigoUnico;
-        return this;
     }
 
-    public Builder descricaoProblema(String descricaoProblema) {
+    public String getDescricaoProblema() {
+        return descricaoProblema;
+    }
+
+    public void setDescricaoProblema(String descricaoProblema) {
         this.descricaoProblema = descricaoProblema;
-        return this;
     }
 
-    public Builder status(StatusOrdemServico status) {
+    public StatusOrdemServico getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusOrdemServico status) {
         this.status = status;
-        return this;
     }
 
-    public Builder valorTotal(Double valorTotal) {
+    public Double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(Double valorTotal) {
         this.valorTotal = valorTotal;
-        return this;
     }
 
-    public Builder dataAbertura(LocalDateTime dataAbertura) {
+    public LocalDateTime getDataAbertura() {
+        return dataAbertura;
+    }
+
+    public void setDataAbertura(LocalDateTime dataAbertura) {
         this.dataAbertura = dataAbertura;
-        return this;
     }
 
-    public Builder dataFechamento(LocalDateTime dataFechamento) {
+    public LocalDateTime getDataFechamento() {
+        return dataFechamento;
+    }
+
+    public void setDataFechamento(LocalDateTime dataFechamento) {
         this.dataFechamento = dataFechamento;
-        return this;
     }
 
-    public Builder dataUltimaAtualizacao(LocalDateTime dataUltimaAtualizacao) {
+    public LocalDateTime getDataUltimaAtualizacao() {
+        return dataUltimaAtualizacao;
+    }
+
+    public void setDataUltimaAtualizacao(LocalDateTime dataUltimaAtualizacao) {
         this.dataUltimaAtualizacao = dataUltimaAtualizacao;
-        return this;
     }
 
-    public Builder servicos(List<Servico> servicos) {
+    public List<Servico> getServicos() {
+        return servicos;
+    }
+
+    public void setServicos(List<Servico> servicos) {
         this.servicos = servicos;
-        return this;
     }
 
-    public Builder pedidosPecas(List<PedidoPecas> pedidosPecas) {
+    public List<PedidoPecas> getPedidosPecas() {
+        return pedidosPecas;
+    }
+
+    public void setPedidosPecas(List<PedidoPecas> pedidosPecas) {
         this.pedidosPecas = pedidosPecas;
-        return this;
     }
-
-    public OrdemServico build() {
-        return new OrdemServico(this);
-    }
-}
-
-public Long getCodigoUnico() {
-    return codigoUnico;
-}
-
-public void setCodigoUnico(Long codigoUnico) {
-    this.codigoUnico = codigoUnico;
-}
-
-public String getDescricaoProblema() {
-    return descricaoProblema;
-}
-
-public void setDescricaoProblema(String descricaoProblema) {
-    this.descricaoProblema = descricaoProblema;
-}
-
-public StatusOrdemServico getStatus() {
-    return status;
-}
-
-public void setStatus(StatusOrdemServico status) {
-    this.status = status;
-}
-
-public Double getValorTotal() {
-    return valorTotal;
-}
-
-public void setValorTotal(Double valorTotal) {
-    this.valorTotal = valorTotal;
-}
-
-public LocalDateTime getDataAbertura() {
-    return dataAbertura;
-}
-
-public void setDataAbertura(LocalDateTime dataAbertura) {
-    this.dataAbertura = dataAbertura;
-}
-
-public LocalDateTime getDataFechamento() {
-    return dataFechamento;
-}
-
-public void setDataFechamento(LocalDateTime dataFechamento) {
-    this.dataFechamento = dataFechamento;
-}
-
-public LocalDateTime getDataUltimaAtualizacao() {
-    return dataUltimaAtualizacao;
-}
-
-public void setDataUltimaAtualizacao(LocalDateTime dataUltimaAtualizacao) {
-    this.dataUltimaAtualizacao = dataUltimaAtualizacao;
-}
-
-public List<Servico> getServicos() {
-    return servicos;
-}
-
-public void setServicos(List<Servico> servicos) {
-    this.servicos = servicos;
-}
-
-public List<PedidoPecas> getPedidosPecas() {
-    return pedidosPecas;
-}
-
-public void setPedidosPecas(List<PedidoPecas> pedidosPecas) {
-    this.pedidosPecas = pedidosPecas;
-}
 }
